f83b48b862e2d3af6f3b87c15eb2055572740ad4
hchang604 <henry.chang@alianza.com>
2024-05-17 19:55:18 -0700

    implement stack nav and bottom tab

--- App.tsx
+++ App.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import ManageExpense from './screens/ManageExpense';
+import RecentExpense from './screens/RecentExpenses';
+import AllExpenses from './screens/AllExpenses';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpenseOverview() {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen name="RecentExpenses" component={RecentExpense} />
+      <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+    </BottomTabs.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpenseOverview" component={ExpenseOverview} />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
---
+++ screens/AllExpenses.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function AllExpenses() {
+  return (
+    <View>
+      <Text>All Expenses</Text>
+    </View>
+  );
+}
+
+export default AllExpenses;
