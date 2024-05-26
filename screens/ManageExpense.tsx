9e7c91714deb8ce3b38de1c2c02ee3d6938e8ff6
hchang604 <henry.chang@alianza.com>
2024-05-26 16:57:18 -0700

    implement button go back functionality

--- screens/ManageExpense.tsx
+++ screens/ManageExpense.tsx
@@ -16,11 +16,17 @@ function ManageExpense() {
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
-  function deleteExpenseHandler() {}
+  function deleteExpenseHandler() {
+    navigation.goBack();
+  }
 
-  function cancelHandler() {}
+  function cancelHandler() {
+    navigation.goBack();
+  }
 
-  function confirmHandler() {}
+  function confirmHandler() {
+    navigation.goBack();
+  }
 
   useLayoutEffect(() => {
     navigation.setOptions({
