dbd7fba32676e899b17d7b674d1755cbfaaf63c7
hchang604 <henry.chang@alianza.com>
2024-06-30 22:12:30 -0700

    implement requests to firebase using axios

---
+++ util/http.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import {ExpenseParams} from '../store/expensesSlice';
+import {Expense} from '../components/ExpensesOutput';
+
+const BACKEND_URL = 'https://expense-tracker-be-default-rtdb.firebaseio.com';
+
+export async function storeExpense(expenseData: ExpenseParams) {
+  const response = await axios.post(BACKEND_URL + '/expense.json', expenseData);
+  const id = response.data.name;
+
+  return id;
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + '/expense.json');
+
+  const expenses: Expense[] = [];
+
+  for (const key in response.data) {
+    const expenseObj: Expense = {
+      id: key,
+      amount: response.data[key].amount,
+      date: response.data[key].date,
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
+}
