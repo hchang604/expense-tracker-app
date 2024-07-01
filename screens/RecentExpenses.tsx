90e0552ab018dea4dfb7a6fbb45778fbb179e1c9
hchang604 <henry.chang@alianza.com>
2024-06-30 22:13:40 -0700

    integrate fetch expenses

--- screens/RecentExpenses.tsx
+++ screens/RecentExpenses.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ExpensesOutput from '../components/ExpensesOutput';
 import {useExpenses} from '../hooks/useExpenses';
+import {fetchExpenses} from '../util/http';
+import {expensesAdded} from '../store/expensesSlice';
+import {useAppDispatch} from '../store/store';
 
 function RecentExpenses() {
   const {recentExpenses} = useExpenses();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses();
+
+      dispatch(expensesAdded(expenses));
+    }
+
+    getExpenses();
+  }, [dispatch]);
 
   return (
     <ExpensesOutput
